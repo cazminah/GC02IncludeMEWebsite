d8dc44661d64592a1eb5b8fdad256254b98c0969
marjisound <Marjan@Marjans-MacBook-Pro.local>
2015-12-12 23:10:55 +0000

    creating tabs for FAQ page

--- Website/js/faq.js
+++ Website/js/faq.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-	console.log("salam");
 	$(".faqItem").click(function(){
 		$(this).next().toggle(250);
-	})
+	});
+
+   $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
 });
